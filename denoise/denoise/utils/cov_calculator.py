864413a4cd6ee1bd67e1ecb7547deebe5d5fc4d5
LISTatSNU <49898081+LISTatSNU@users.noreply.github.com>
2023-03-30 18:13:13 +0900

    Add files via upload

--- denoise/denoise/utils/cov_calculator.py
+++ denoise/denoise/utils/cov_calculator.py
@@ -1,5 +1,5 @@
 import torch
-
+"""
 def calculate(noisy, sen, in_arr, out_arr):
     noisy_bg = torch.cat((noisy[:,:,:20,:20],
                         noisy[:,:,-20:,:20],
@@ -27,40 +27,40 @@ def calculate(noisy, sen, in_arr, out_arr):
 
 
 """
-for coil by correlation..... poor result
-"""
-# def calculate(noisy, sen, in_arr, out_arr):
-#     noisy_bg = torch.cat((noisy[:,:,:20,:20],
-#                         noisy[:,:,-20:,:20],
-#                         noisy[:,:,:20,-20:],
-#                         noisy[:,:,-20:,-20:]),3)
-    
-#     noisy_mean = noisy_bg.mean([1,2,3])
-    
-#     cov_map = torch.zeros_like(sen[0:3,...])
-    
-#     for i in in_arr:
-#         sen_i = sen[i,...] 
-#         for j in out_arr:
-#             sen_j = sen[j,...] 
-#             cov11 = torch.var(noisy_bg[i,...])
-#             cov12 = torch.mean((noisy_bg[i,...] - noisy_mean[i])*(noisy_bg[j,...] - noisy_mean[j]))
-#             cov22 = torch.var(noisy_bg[j,...])
-#             cov_map[0,...] = cov_map[0,...] + cov11 * torch.abs(sen_i) * torch.abs(sen_j)
-#             cov_map[1,...] = cov_map[1,...] + cov12 * torch.abs(sen_i) * torch.abs(sen_j)
-#             cov_map[2,...] = cov_map[2,...] + cov22 * torch.abs(sen_i) * torch.abs(sen_j)
-    
-#     alpha = - cov_map[1,...] / torch.sqrt(cov_map[0,...] * cov_map[2,...] - cov_map[1,...] ** 2 + 1e-12)
 
-#     if alpha.max() == 0:
-#         beta = torch.ones_like(alpha)
 
-#     else:
-#         beta = cov_map[0,...] / torch.sqrt(cov_map[0,...] * cov_map[2,...] - cov_map[1,...] ** 2 + 1e-12)
+def calculate(noisy, sen, in_arr, out_arr):
+    noisy_bg = torch.cat((noisy[:,:,:20,:20],
+                        noisy[:,:,-20:,:20],
+                        noisy[:,:,:20,-20:],
+                        noisy[:,:,-20:,-20:]),3)
+    
+    noisy_mean = noisy_bg.mean([1,2,3])
+    
+    cov_map = torch.zeros_like(sen[0:3,...])
+    
+    for i in in_arr:
+        sen_i = sen[i,...] 
+        for j in out_arr:
+            sen_j = sen[j,...] 
+            cov11 = torch.var(noisy_bg[i,...])
+            cov12 = torch.mean((noisy_bg[i,...] - noisy_mean[i])*(noisy_bg[j,...] - noisy_mean[j]))
+            cov22 = torch.var(noisy_bg[j,...])
+            cov_map[0,...] = cov_map[0,...] + cov11 * torch.abs(sen_i) * torch.abs(sen_j)
+            cov_map[1,...] = cov_map[1,...] + cov12 * torch.abs(sen_i) * torch.abs(sen_j)
+            cov_map[2,...] = cov_map[2,...] + cov22 * torch.abs(sen_i) * torch.abs(sen_j)
+    
+    alpha = - cov_map[1,...] / torch.sqrt(cov_map[0,...] * cov_map[2,...] - cov_map[1,...] ** 2 + 1e-12)
 
-#     print(torch.abs(alpha).max(),end = ' ')
-#     a = noisy_bg[in_arr,...]
-#     b = noisy_bg[out_arr,...]
-#     cor =  torch.mean((a - a.mean())*(b - b.mean()))
-#     print(cor / torch.sqrt(torch.var(a) * torch.var(b) - torch.mean((a - a.mean())*(b - b.mean())) ** 2) )
-#     return alpha, beta
+    if alpha.max() == 0:
+        beta = torch.ones_like(alpha)
+
+    else:
+        beta = cov_map[0,...] / torch.sqrt(cov_map[0,...] * cov_map[2,...] - cov_map[1,...] ** 2 + 1e-12)
+
+    print(torch.abs(alpha).max(),end = ' ')
+    a = noisy_bg[in_arr,...]
+    b = noisy_bg[out_arr,...]
+    cor =  torch.mean((a - a.mean())*(b - b.mean()))
+    print(cor / torch.sqrt(torch.var(a) * torch.var(b) - torch.mean((a - a.mean())*(b - b.mean())) ** 2) )
+    return alpha, beta
